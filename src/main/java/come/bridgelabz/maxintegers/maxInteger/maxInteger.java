606edabf2c3820f6c2fa4cebff5df92f5e0afce9
Praneethachodey <praneethachodey@gmail.com>
2020-10-09 23:59:34 +0530

    [Praneetha]Added: test for maximum floats at diff positions

--- src/main/java/come/bridgelabz/maxintegers/maxInteger/maxInteger.java
+++ src/main/java/come/bridgelabz/maxintegers/maxInteger/maxInteger.java
@@ -2,14 +2,24 @@ package come.bridgelabz.maxintegers.maxInteger;
 
 public class maxInteger {
 
-	public static Integer maxInt(Integer one,Integer two,Integer three)
-	{
-		Integer max=one;
-		if(two.compareTo(max)>0)
-			max=two;
-		if(three.compareTo(max)>0)
-			max=three;
+	public static Integer maxInt(Integer one, Integer two, Integer three) {
+		Integer max = one;
+		if (two.compareTo(max) > 0)
+			max = two;
+		if (three.compareTo(max) > 0)
+			max = three;
 		return max;
-		
+
 	}
+
+	public static Float maxFloat(Float one, Float two, Float three) {
+		Float max = one;
+		if (two.compareTo(max) > 0)
+			max = two;
+		if (three.compareTo(max) > 0)
+			max = three;
+		return max;
+
+	}
+
 }
--- src/test/java/come/bridgelabz/maxintegers/maxInteger/maxIntegerTest.java
+++ src/test/java/come/bridgelabz/maxintegers/maxInteger/maxIntegerTest.java
@@ -3,23 +3,43 @@ package come.bridgelabz.maxintegers.maxInteger;
 import org.junit.*;
 
 public class maxIntegerTest {
+
+	@Test
+	public void maxIntAtOne() {
+		Integer max = maxInteger.maxInt(67, 10, 25);
+		Assert.assertSame(max, 67);
+	}
+
+	@Test
+	public void maxIntAtTwo() {
+		Integer max = maxInteger.maxInt(10, 67, 25);
+		Assert.assertSame(max, 67);
+	}
+
+	@Test
+	public void maxIntAtThree() {
+		Integer max = maxInteger.maxInt(10, 25, 67);
+		Assert.assertSame(max, 67);
+	}
 	
-@Test
-public void maxAtOne() {
-	Integer max = maxInteger.maxInt(67, 10, 25);
-	Assert.assertSame(max,67);
-}
-
-@Test
-public void maxAtTwo() {
-	Integer max = maxInteger.maxInt(10, 67, 25);
-	Assert.assertSame(max,67);
-}
-
-@Test
-public void maxAtThree() {
-	Integer max = maxInteger.maxInt(10, 25, 67);
-	Assert.assertSame(max,67);
-}
+	@Test
+	public void maxFloatAtFirst() {
+		Float max = maxInteger.maxFloat(67.25f, 10.22f, 25.67f);
+		Assert.assertEquals(max,67.25,0.0);
+	}
+	
+	@Test
+	public void maxFloatAtTwo() {
+		Float max = maxInteger.maxFloat(10.22f, 67.25f, 25.67f);
+		Assert.assertEquals(max,67.25,0.0);
+	}
+	
+	@Test
+	public void maxFloatAtThree() {
+		Float max = maxInteger.maxFloat(10.22f, 25.67f, 67.25f);
+		Assert.assertEquals(max,67.25,0.0);
+	}
+	
+	
 
 }
